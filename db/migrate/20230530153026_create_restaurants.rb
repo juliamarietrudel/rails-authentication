e85f8dde2dd4db59d075dfddee1facc50e502e7f
Julia Trudel <juliamarietrudel96@gmail.com>
2023-05-30 11:54:25 -0400

    added username

--- db/migrate/20230530153026_create_restaurants.rb
+++ db/migrate/20230530153026_create_restaurants.rb
@@ -1,7 +1,7 @@
 class CreateRestaurants < ActiveRecord::Migration[7.0]
   def change
     create_table :restaurants do |t|
-      t.string :name
+      t.string :username
 
       t.timestamps
     end
